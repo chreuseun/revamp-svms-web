15ffe3b007420d4812dc32cd2b450a0997e96753
Eunille <eunille.villegas@growsari.com>
2023-01-21 12:18:37 +0800

    eslintrc set no-unused-vars to "warn"

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,5 +27,6 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'no-unused-vars': 'warn',
   },
 };
