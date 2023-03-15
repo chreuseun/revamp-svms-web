48b51915df7264cdbc461b1162af9598e04f8670
Eunille <eunille.villegas@growsari.com>
2023-03-15 10:29:31 +0800

    uupdate endpoint

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,6 +1,6 @@
-// const heroku = 'https://dev-svms-express-server.herokuapp.com';
-const local = 'http://192.168.100.2:3000';
-const DEFAULT_BASE_URL = local; // heroku || local;
+const heroku = 'https://dev-svms-express-server.herokuapp.com';
+// const local = 'http://192.168.100.2:3000';
+const DEFAULT_BASE_URL = heroku; // heroku || local;
 
 // ENDPOINTS LIST
 const ENDPOINTS = {
