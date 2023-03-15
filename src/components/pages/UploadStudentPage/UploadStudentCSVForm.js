fd5ca212fe4348306aaa4b9e5d712e00a81a12f3
Eunille <eunille.villegas@growsari.com>
2023-03-15 14:43:11 +0800

    add data csv reading

--- src/components/pages/UploadStudentPage/UploadStudentCSVForm.js
+++ src/components/pages/UploadStudentPage/UploadStudentCSVForm.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import { Form, Upload, Button, Spin } from 'antd';
+import { Form, Upload, Button, Spin, message, Typography } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import { useCSVImporter } from 'src/hooks/csv';
 import { STUDENT_UPLOAD_FORM } from 'src/constants/students';
 
 const { useForm } = Form;
-const { UPLOAD_STUDENT_CSV } = STUDENT_UPLOAD_FORM;
+const { UPLOAD_STUDENT_CSV, FORM_NAME } = STUDENT_UPLOAD_FORM;
 
 const UploadStudentCSVForm = () => {
   const [uploadStudentRecordsForm] = useForm();
@@ -20,14 +20,35 @@ const UploadStudentCSVForm = () => {
   };
 
   const onRemoveFile = () => {
+    uploadStudentRecordsForm.setFieldValue(UPLOAD_STUDENT_CSV.name, null);
     setCSVData(null);
   };
+
+  const onFinish = values => {
+    if (!csvData) {
+      message.error('CSV Data is required');
+    }
+  };
+
+  const studentRecordCount = csvData?.rows?.length || 0;
+  const columnNames = csvData?.columnNames || [];
 
   return (
     <Spin spinning={isImportingCSV}>
       <div style={styles.container}>
-        <Form form={uploadStudentRecordsForm}>
-          <Form.Item {...UPLOAD_STUDENT_CSV}>
+        <div>
+          <Typography.Text type="success">Column names:</Typography.Text>
+          <div style={{ marginLeft: 16 }}>
+            {columnNames.map(col => (
+              <div key={col}>{col || ''}</div>
+            ))}
+          </div>
+        </div>
+        <Typography.Text type="success">Record count:</Typography.Text>
+        <div style={{ marginLeft: 16 }}>{studentRecordCount}</div>
+
+        <Form form={uploadStudentRecordsForm} name={FORM_NAME} onFinish={onFinish}>
+          <Form.Item {...UPLOAD_STUDENT_CSV} re>
             <Upload.Dragger
               maxCount={1}
               multiple={false}
@@ -38,8 +59,12 @@ const UploadStudentCSVForm = () => {
               <Button icon={<UploadOutlined />}>Click to Upload</Button>
             </Upload.Dragger>
           </Form.Item>
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
         </Form>
-        <pre>{JSON.stringify(csvData, null, 4)}</pre>
       </div>
     </Spin>
   );
--- src/constants/students.js
+++ src/constants/students.js
@@ -1,4 +1,5 @@
 const STUDENT_UPLOAD_FORM = {
+  FORM_NAME: 'student-csv-upload',
   UPLOAD_STUDENT_CSV: {
     label: 'Upload Student Record',
     name: 'students_csv',
