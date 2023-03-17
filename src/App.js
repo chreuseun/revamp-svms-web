32988b5bc461e8323cecc27280bb47b2f4bf3dbd
Eunille <eunille.villegas@growsari.com>
2023-03-17 10:52:47 +0800

    add build date

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import { Provider } from 'react-redux';
 import store from 'src/redux/store';
 import RoutedApp from 'src/RoutedApp';
 import packageJSON from '../package.json';
+import buildConfig from 'src/buildConfig';
 
 const App = () => (
   <Provider store={store}>
     <div style={{ fontSize: 7, position: 'absolute', bottom: 0, right: 0 }}>
-      {process.env?.NODE_ENV || ''}@{packageJSON?.version}
+      {process.env?.NODE_ENV || ''}@{packageJSON?.version}-{buildConfig.BUILD_DATE}
     </div>
     <RoutedApp />
   </Provider>
