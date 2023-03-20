ac269a0330b4f616d24c8d8ebb3ff3b6e40dca82
Eunille <eunille.villegas@growsari.com>
2023-03-20 15:11:56 +0800

    add fetching data from API to FE in MyDepartments Page

--- src/components/pages/users/MyDepartmentsListPage/MyDepartmentsListPage.js
+++ src/components/pages/users/MyDepartmentsListPage/MyDepartmentsListPage.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { DefaultContainer, NavigationSidebar, PageContentContainer } from 'src/components/common';
+import { userAPIForDepartments } from 'src/hooks/APIs/users';
+
+const { useGETDepartmentsListByAccountID } = userAPIForDepartments;
 
 const MyDepartmentsListPage = () => {
+  const [myDepartmentList, setMyDepartmentList] = useState([]);
+  const { runGETDepartmentsListByAccountID, isGETDepartmentsListByAccountIDLoading } =
+    useGETDepartmentsListByAccountID({
+      onCompleted: (list = []) => {
+        if (Array.isArray(list)) {
+          setMyDepartmentList(list || []);
+        }
+      },
+    });
+
+  useEffect(() => {
+    runGETDepartmentsListByAccountID();
+  }, []);
+
   return (
-    <DefaultContainer customStyles={styles.container} isLoading={false}>
+    <DefaultContainer
+      customStyles={styles.container}
+      isLoading={isGETDepartmentsListByAccountIDLoading}>
       <NavigationSidebar />
-      <PageContentContainer
-        title="My Departments"
-        containerStyles={styles.pageContentContainer}></PageContentContainer>
+      <PageContentContainer title="My Departments" containerStyles={styles.pageContentContainer}>
+        <div style={{ border: '2px solid red' }}>
+          {myDepartmentList.map(departmentDetails => (
+            <div
+              key={departmentDetails.v2_department_id}
+              style={{
+                border: '2px solid #363636',
+                borderRadius: 8,
+                backgroundColor: '#EFEFEF',
+                padding: '10px 16px',
+                marginBottom: 8,
+              }}>
+              {departmentDetails?.v2_departments_name}
+            </div>
+          ))}
+        </div>
+        <pre style={{ fontSize: 8 }}>{JSON.stringify(myDepartmentList, null, 4)}</pre>
+      </PageContentContainer>
     </DefaultContainer>
   );
 };
